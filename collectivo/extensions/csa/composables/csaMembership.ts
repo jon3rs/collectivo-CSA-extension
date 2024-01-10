398b6497ca745a4eff88af4e2e11abbbf544c42b
Jonas Anetzberger <jonas.anetzberger@smail.th-koeln.de>
2024-01-10 13:22:29 +0100

    having troubles accessing directus

--- collectivo/extensions/csa/composables/csaMembership.ts
+++ collectivo/extensions/csa/composables/csaMembership.ts
@@ -6,7 +6,7 @@ import {
 
 export async function getCSAmemberships(){
     const directus = await useDirectus();
-
+    console.log(useDirectus());
     const memberships = await directus.request(readItems("csa_membership"));
 
     return memberships;
